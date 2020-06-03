110f1fea243c8e7817ec2d0b7eaa21f431c1852f
kiara1404 <kiarabroekhuizen1@hotmail.com>
2020-06-03 09:40:29 +0200

    connected with mongoDB and added profiles

--- index.js
+++ index.js
@@ -4,10 +4,29 @@ const port = 8000;
 const find = require('array-find');
 const bodyParser = require("body-parser"); 
 const slug = require("slug");
+const mongo = require('mongodb')
 
-/* set templating engine */
+require('dotenv').config();
+
+//database connect
+var db = null;
+var url = 'mongodb+srv://' + process.env.DB_HOST;
+
+mongo.MongoClient.connect(url, 
+    { useUnifiedTopology: true, },
+    function (err, client) {
+    if (err){
+         throw err
+    }
+    db = client.db(process.env.DB_NAME);
+    console.log('Succesfully connected to MongoDB')
+
+}
+);
+
+//set templating engine 
 app.set('view engine', 'ejs');
-/* where are the templates stored */
+// where are the templates stored
 app.set('views', 'view');
 //
 app.use(bodyParser.urlencoded({extended:true}))
@@ -17,6 +36,10 @@ app.use(express.static('public'));
 //app.use(notFound);
 app.post('/', add)
 app.get('/add', form)
+app.get('/:id', person)
+app.listen(port, function(){
+    console.log('The  server is running')
+});
 
 
 function index(req, res) {
@@ -34,56 +57,23 @@ function notFound (req, res) {
 function form(req, res) {
    res.render('add');
 }
-
-function add(req, res) {
-    var id = slug(req.body.name).toLowerCase();
-
-    data.push({
-    id: id,
+function add(req,res,next){
+    db.collection("getfit").insertOne({
     name: req.body.name,
     age: req.body.age,
     place: req.body.place,
     bio: req.body.bio
-    })
+    }, done)
 
-res.redirect('/people')
-console.log(data.length)
+    function done(err, data) {
+        if(err) {
+            next(err)
+        } else {
+            res.redirect('/'+ data.insertedId)
+        }
+    }
 }
 
-app.listen(port, function(){
-    console.log('The  server is running')
-});
-
-
-
-
-const data = [
-    {
-    id: "sanne",
-    name: "Sanne",
-    age:"27",
-    place:"Amsterdam",
-    bio: "I just started my journey. I work fulltime so I find it hard to workout during the week. I am looking for someone who can hold me accountable and pushes me.",
-    },
-    {
-    id: "gaby",
-    name: "Gaby",
-    age:"22",
-    place:"Alphen aan den Rijn",
-    bio: "I have been doing fitness for two years now. I am very motivated and I like to workout 5 days a week. I am looking for someone I can help as I've had a friend who's helped me starting this journey.",
-    },
-    {
-    id: "tim",
-    name: "Tim",
-    age:"24",
-    place:"Rotterdam",
-    bio: "I am a football trainer at Feyenoord. I love to help people improve their overall health. If you'd like to improve your cardio, hit me up!",
-     },
-     {
-    id: "giordano", 
-    name: "Giordano",
-    age:"21",
-    place:"Rotterdam",
-    bio: "I've tried nummerous times to lose weight but I find it very diffult. I am overweight and I never know what to do in the gym, so often I just leave after 15 minutes on the elliptical. I am looking for some guidance.",
-    }
-]
+function person(req, res) {
+    res.render('detail');
+}
